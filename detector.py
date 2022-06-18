137c6e1bc6d588a2e2d878017255a91c3f3fe383
rahulramesh96 <cerebralblade@gmail.com>
2022-06-18 19:55:35 -0400

    Malware check done

--- detector.py
+++ detector.py
@@ -4,6 +4,7 @@ from scapy.all import *
 import sys
 import os
 import hashlib
+import glob
 
 
 print("Welcome to Netmon, the malware detector!")
@@ -24,8 +25,57 @@ def main():
         path = str(subprocess.check_output(['pwd'], shell = False))
         new_path = path[2:-3]
         print(new_path)
-        print("The present working directory is: ", new_path)
-        os.system("cd "+ new_path)
+        file = glob.glob(new_path + "/*.*")
+        
+        print()
+        print('File name'+ '\t\t' + 'SHA-256 Hash')
+        for f in file:
+            with open(f, 'rb') as getsha256:
+                data = getsha256.read()
+                gethash = hashlib.sha256(data).hexdigest()
+                file_name = os.path.basename(new_path)
+                
+                print( f + '\t\t' + gethash)
+        print()
+        print("All SHA-256 hashes performed.")
+        print()
+        hash_database = os.path.exists('malware_hash_database.txt')
+        if hash_database is False:
+            os.system('touch malware_hash_database.txt')
+
+        fake_malware = os.path.exists('my_fake_malware.txt')
+        if fake_malware is False:    
+            os.system('touch my_fake_malware.txt')
+        
+        malware_hash = hashlib.sha256(open('my_fake_malware.txt','rb').read()).hexdigest()
+        print("Malware's SHA-256 Hash is: " + malware_hash)
+
+        text_file = open("./malware_hash_database.txt", "w")
+        text_file.write(malware_hash + ':' + 'fake_malware')
+        text_file.close()
+
+        def check_if_string_in_file(file_name, string_to_search):
+            """ Check if any line in the file contains given string """
+            # Open the file in read only mode
+            with open(file_name, 'r') as read_obj:
+                # Read all lines in the file one by one
+                for line in read_obj:
+                    # For each line, check if line contains the string
+                    if string_to_search in line:
+                        print(line + " is found in malware database")
+                        print("Quarantine! or take appropriate action.")
+            return False
+        
+        check_if_string_in_file('malware_hash_database.txt', gethash)
+
+
+
+
+
+
+
+
+        
 
 
 if ("--help" or "-h") in sys.argv:
