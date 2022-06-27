1a8e9b05b9113e64c6e69ed7fb605e842efc94ba
rahulramesh96 <cerebralblade@gmail.com>
2022-06-26 20:06:21 -0400

    Final changes made

--- detector.py
+++ detector.py
@@ -96,110 +96,106 @@ def main(): # This is the main function where the entire code for the program re
 
         # Below is the process of performing the malware scan using threading
         print('Malware Scan with THREADING')
-        start2 = time.time() 
-        x = threading.Thread(target = mal_scan, args = ('malware_hash_database.txt', gethash))
-        x.start()
+        start2 = time.time() # We start the timer to compute the elapsed execution time, 'start2' variable holds the elapsed time in runtime.
+        x = threading.Thread(target = mal_scan, args = ('malware_hash_database.txt', gethash)) # Making use of threading here to speed up the process. the target function is mal_scan to call. The arguments are passed in as well.
+        x.start() # x is the variable that holds the thread. The thread x is started.
         
-        print('Active threads: ' , threading.active_count())
-        end2 = time.time()
-        final_time_2 = end2 - start2
-        print('scan completed in ', final_time_2 , 'seconds')
+        print('Active threads: ' , threading.active_count()) # This prints out the number of active threads.
+        end2 = time.time() # 'end2' variable holds the value of elapsed time in runtime.
+        final_time_2 = end2 - start2 # 'final_time_2' gives the value of the total time in seconds by subtracting the end time and the start time.
+        print('scan completed in ', final_time_2 , 'seconds') # The total execution time using threading is shown.
 
-
-
-    if "--net-mon" in sys.argv:
-        print("Welcome to Network Monitoring tool!")
-        interface = sys.argv[sys.argv.index("--net-mon")+1]
+    if "--net-mon" in sys.argv: # When the input contains the '--net-mon' switch.
+        print("Welcome to Network Monitoring tool!") # Prints the welcome message.
+        interface = sys.argv[sys.argv.index("--net-mon")+1] # The interface is taken as the immediate next CLI argument after the '--net-mon' switch
         
-        if interface not in conf.iface:
-            print("Cannot proceed. Enter an active interface!")
-            exit()
-        ip = get_if_addr(interface)
-        print("Your IP address of "+ interface + " is " + ip)
+        if interface not in conf.iface: # If the interface specified in the argument is not valid.
+            print("Cannot proceed. Enter an active interface!") # The program will exit and would not proceed.
+            exit() # The program exits post the error.
+        ip = get_if_addr(interface) # We get the ip address of the interface and store it in the variable 'ip'
+        print("Your IP address of "+ interface + " is " + ip) # The IP address of the resepective interface is printed.
 
-        pkt_count = 0
-        pkt_to_sniff = pkt_count
+        pkt_to_sniff = 0 # The pkt_sniff is set to 0
 
-        # This program will capture only the ARP packets.
+        # Below code will capture the ARP packets on the specified interface.
         
+        bad_ips = os.path.exists('bad_ips.txt') # Checks if the file bad_ips.txt is present in the given location.
+        if bad_ips is False: # If the file is not present, the file will be created and be inserted with few sample bad IP Addresses.
+            os.system('touch bad_ips.txt ; sudo chmod 755 bad_ips.txt') # We create the bad_ips.txt file and give a file permission of u=rwx, g=rx, o=rx
+            os.system('echo "10.0.2.5" >> bad_ips.txt')  #sample bad IP Address 1
+            os.system('echo "10.0.2.30" >> bad_ips.txt') #sample bad IP Address 2
+            os.system('echo "10.0.2.100" >> bad_ips.txt') #sample bad IP Address 3
+        bad_domains = os.path.exists('bad_domains.txt') # Check is the bad_domains.txt file is present in the given location.
+        if bad_domains is False: # If the file is not present, the file will be created and be inserted with few sample IP Addresses.
+            os.system('touch bad_domains.txt ; sudo chmod 755 bad_domains.txt') # We create the file called bad_domains.txt and give a file permission of u=rwx, g=rx, o=rx
+            os.system('echo "www.google.com" > bad_domains.txt') # sample malicious domain 1 is google.com
 
-
-        bad_ips = os.path.exists('bad_ips.txt')
-        if bad_ips is False:
-            os.system('touch bad_ips.txt ; sudo chmod 777 bad_ips.txt')
-            os.system('echo "10.0.2.5" >> bad_ips.txt')  #sample IP Address 1
-            os.system('echo "10.0.2.30" >> bad_ips.txt') #sample IP Address 2
-            os.system('echo "10.0.2.100" >> bad_ips.txt') #sample IP Address 3
-        bad_domains = os.path.exists('bad_domains.txt')
-        if bad_domains is False:
-            os.system('touch bad_domains.txt ; sudo chmod 777 bad_domains.txt')
-            os.system('echo "www.google.com" > bad_domains.txt')
         
-
-        proto_sniff = "arp"
+        proto_sniff = "arp" # We will set the proto_sniff variable to arp protocol
         # The function will extract parameters from the packet and then log each packet
-        def packet_log(packet):
-            
-
+        def packet_log(packet): # The function packet_log takes in 'packet' as a parameter
 
         # Writing the packet information to the log file
-            if(proto_sniff == "arp"):
-            # Writing the data to the console output the file
+            if(proto_sniff == "arp"): # As we are sniffing only in the arp protocol.
+            # Writing the data to console output the file
                 
-                print(packet[0].psrc + "\t\t" + packet[0].pdst)
-                bad_ips= open("bad_ips.txt", "r")
-                bad_ips_content = bad_ips.read()
-                if (packet[0].psrc) in bad_ips_content:
-                    print('\x1b[7;31;50m' + packet[0].psrc + '\t\t' + 'ALERT! Malicious IP found.' + '\x1b[0m')
+                print(packet[0].psrc + "\t\t" + packet[0].pdst) # The source IP Address and the destination IP Address of the packet is shown
+                bad_ips= open("bad_ips.txt", "r") # The bad_ips.txt file is read.
+                bad_ips_content = bad_ips.read() # The bad_ips is read and stored in the variable bad_ips_content.
+                if (packet[0].psrc) in bad_ips_content: # We check with an if statement if the source ip address ofthe packet is present in the bad_ips list.
+                    print('\x1b[7;31;50m' + packet[0].psrc + '\t\t' + 'ALERT! Malicious IP found.' + '\x1b[0m') # This will print the IP address of the malicious machine in the network.
 
         # Printing an informational message to the screen
-        print("\n* Starting the capture...")
+        print("\n* Starting the capture...") 
         print(" Source IP " + "\t\t" + " Destination IP ")
 
         # Running the sniffing process (with or without a filter)
                 
 
-        if proto_sniff == "arp":
-            sniff(iface=interface, filter=proto_sniff, count=int(pkt_to_sniff), prn=packet_log, timeout = 1)
+        if proto_sniff == "arp": # We will sniff only the packets that have ARP protocol.
+            sniff(iface=interface, filter=proto_sniff, count=int(pkt_to_sniff), prn=packet_log, timeout = 2) # This is the sniff function from scapy which will functions as a passive scan for 20 seconds.
             print("End of Capture.")
             print()
-            
 
         else:
-            print("\nCould not identify the protocol.\n")
+            print("\nCould not identify the protocol.\n") # If no ARP packets were sniffed, this will return with an error.
             exit()  
 
 
-        packet = IP(dst="1.1.1.1") / UDP() / DNS(rd=1, qd=DNSQR(qname="www.google.com"))
+        packet = IP(dst="1.1.1.1") / UDP() / DNS(rd=1, qd=DNSQR(qname="www.google.com")) # We can try sending a DNS Query with the domain name www.google.com.
+ 
         
-        domain_name = str(packet.qd.qname)
-        domain = str(domain_name[1:])
-        print('Visited Domain ' + domain)
+        domain_name = str(packet.qd.qname) # This will convert the qname 'www.google.com' to string and store it in the domain_name variable.
+        domain = str(domain_name[1:]) # This will strip off the character 'b' in the domain_name variable
+        print('Queried Domain ' + domain) # The queried domain will be printed here.
         
-        bad_domains= open("bad_domains.txt", "r")
-        bad_domains_content = bad_domains.read()  
-        if domain in bad_domains_content:
+        bad_domains= open("bad_domains.txt", "r") # The bad domains text file will be opened in read mode.
+        bad_domains_content = bad_domains.read()  # The bad_domains_content will store each line bad_domains.txt
+        if domain in bad_domains_content: # The bad_domains_content will be checked with the queried domain name in the below loop.
 
             print('Reading through dangerous domains...')
-            print(bad_domains_content)
+            print(bad_domains_content) # This will print out the list of malicious domains.
             print()
             print('Domain' + '\t\t\t\t' + 'Description')
             print()
-            print('\x1b[7;31;50m' + domain + '\t\t' + 'ALERT! Malicious Domain found.' + '\x1b[0m')
+            try:
+                print('\x1b[7;31;50m' + domain + '\t\t' + 'ALERT! Malicious Domain found.' + '\x1b[0m')
+            except:
+                print('No malicious domains visited.')
       
-if ("--help") in sys.argv:
-    if len(sys.argv)==2:
-        help()
+if ("--help") in sys.argv: # If '--help' switch is present in the CLI argument, the length of the CLI arguments are collected
+    if len(sys.argv)==2: # The length of the arguments are checked to see if there is no collision.
+        help() # The help function is called.
     else:
-        print("Do not use more than one switch for help.")
+        print("Do not use more than one switch for help.") # Prints out to display only -h or --help can be used as a switch for help.
         exit()
 
-if ("-h") in sys.argv:
-    if len(sys.argv)==2:
-        help()
+if ("-h") in sys.argv: # If '-h' switch is present in the CLI argument, the length of the CLI arguments are collected
+    if len(sys.argv)==2: # The length of the arguments are checked to see if there is no collision.
+        help() # The help function is called.
     else:
-        print('Do not use more than one switch for help.')
+        print('Do not use more than one switch for help.') # Prints out to display only -h or --help can be used as a switch for help.
         exit()
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": # Checks if any of the function's name is main, if yes, then enter this loop.
+    main() # This is the main function which will be executed first when the program is executed.
